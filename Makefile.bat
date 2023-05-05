b08b67b3d5cf12289137d467a3b2fec2c74637a4
d-lombardi-a <53909185+d-lombardi-a@users.noreply.github.com>
2023-05-05 22:19:21 +0200

    Update Makefile.bat

--- Makefile.bat
+++ Makefile.bat
@@ -1,7 +1,7 @@
-mkdir send2google_earth
-xcopy *.py send2google_earth
-xcopy README.md send2google_earth
-xcopy metadata.txt send2google_earth
-zip -r send2google_earth.zip send2google_earth
-del /Q send2google_earth
-rd send2google_earth
+mkdir send2World_Imagery_Wayback
+xcopy *.py send2World_Imagery_Wayback
+xcopy README.md send2World_Imagery_Wayback
+xcopy metadata.txt send2World_Imagery_Wayback
+zip -r send2World_Imagery_Wayback.zip send2World_Imagery_Wayback
+del /Q send2World_Imagery_Wayback
+rd send2World_Imagery_Wayback
